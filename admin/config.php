ccd0fa57c8bbdc6171755b4b4683332949c20573
JJkalnins <jekabs.kalnins@indeed.pro>
2017-01-30 13:25:16 +0200

    Clean copona

---
+++ admin/config.php
@@ -0,0 +1,34 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/copona/admin/');
+define('HTTP_CATALOG', 'http://localhost/copona/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/copona/admin/');
+define('HTTPS_CATALOG', 'http://localhost/copona/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/UniServerZ/www/copona/admin/');
+define('DIR_SYSTEM', 'C:/UniServerZ/www/copona/system/');
+define('DIR_IMAGE', 'C:/UniServerZ/www/copona/image/');
+define('DIR_LANGUAGE', 'C:/UniServerZ/www/copona/admin/language/');
+define('DIR_TEMPLATE', 'C:/UniServerZ/www/copona/admin/view/template/');
+define('DIR_CONFIG', 'C:/UniServerZ/www/copona/system/config/');
+define('DIR_CACHE', 'C:/UniServerZ/www/copona/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/UniServerZ/www/copona/system/storage/download/');
+define('DIR_LOGS', 'C:/UniServerZ/www/copona/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/UniServerZ/www/copona/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/UniServerZ/www/copona/system/storage/upload/');
+define('DIR_CATALOG', 'C:/UniServerZ/www/copona/catalog/');
+
+// CACHE
+define('CACHE_EXPIRE', 3600);
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'copona');
+define('DB_PASSWORD', 'copona');
+define('DB_DATABASE', 'copona');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
